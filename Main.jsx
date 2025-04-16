fa54233c1895403bf2523cd1815dbd0500b86677
pyry.rautalin@gmail.com <RANDOM_EMAIL>
2025-04-16 23:12:53 +0300

    first version of adding data to an array via form

--- Main.jsx
+++ Main.jsx
@@ -1,10 +1,31 @@
-const Main = () => (
+const Main = () => {
+
+    const ingredientsList = ["Chicken, Oregano, Tomatoes, Cheese"]
+
+    const ingredientListItems = ingredientsList.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+
+    ))
+
+    function formSubmitted(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredientsList.push(newIngredient)
+        console.log(ingredientsList)
+    }
+    
+return (
+    <>
     <div className="formcontainer">
-    <form className="addingridientform">
-        <input type="text" placeholder="Onion" aria-label="Add an ingredient" />
+    <form onSubmit={formSubmitted} className="addingridientform" acceptCharset="utf-8">
+        <input type="text" placeholder="type and ingredient to add for your recipe e.g. Onion" aria-label="Add an ingredient" name="ingredient" />
         <button>Add an ingredient</button>
     </form>
     </div>
+    <ul>{ingredientListItems}</ul>
+    </>
 )
+}
 
 export default Main
