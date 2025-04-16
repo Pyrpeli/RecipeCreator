15435cdb8456b194e4298a86202b4f5cc8cf608d
pyry.rautalin@gmail.com <RANDOM_EMAIL>
2025-04-16 14:33:32 +0300

    initial commit, Header in place

---
+++ Header.jsx
@@ -0,0 +1,14 @@
+import chefLogo from "./images/chef-claude-icon.png"
+
+const Header = () => (
+    <div className="header">
+        <div className="headerbar">
+        <img className="logo" src={chefLogo} />
+        <span>Chef Claude</span>
+        </div>
+        
+    </div>
+
+)
+
+export default Header
