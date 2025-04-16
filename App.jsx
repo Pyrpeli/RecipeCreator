687059f7495b0089c918f57276cbd755e6d1b527
pyry.rautalin@gmail.com <RANDOM_EMAIL>
2025-04-16 15:26:50 +0300

    Add form elements for adding ingredients

--- App.jsx
+++ App.jsx
@@ -1,8 +1,10 @@
 import Header from "./Header"
+import Main from "./Main"
 
 const App = () => (
   <>
   <Header />
+  <Main />
   </>
 )
 
---
+++ Main.jsx
@@ -0,0 +1,10 @@
+const Main = () => (
+    <div className="formcontainer">
+    <form className="addingridientform">
+        <input type="text" placeholder="Onion" aria-label="Add an ingredient" />
+        <button>Add an ingredient</button>
+    </form>
+    </div>
+)
+
+export default Main
