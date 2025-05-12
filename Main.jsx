b014aa867083939d4af1e13a7b5dd8e72c93b7e5
pyry.rautalin@gmail.com <RANDOM_EMAIL>
2025-05-12 19:51:43 +0300

    simplify form submition

--- Main.jsx
+++ Main.jsx
@@ -10,19 +10,16 @@ const Main = () => {
 
     ))
 
-    function formSubmitted(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function submitIngredient(formData) {
         const newIngredient = formData.get("ingredient")
         addIngredient(prevIngredients => [...prevIngredients, newIngredient])
-        ingredientsList.push(newIngredient)
-        console.log(ingredientsList)
+
     }
     
 return (
     <>
     <div className="formcontainer">
-    <form onSubmit={formSubmitted} className="addingridientform" acceptCharset="utf-8">
+    <form action={submitIngredient} className="addingridientform" acceptCharset="utf-8">
         <input type="text" placeholder="type and ingredient to add for your recipe e.g. Onion" aria-label="Add an ingredient" name="ingredient" />
         <button>Add an ingredient</button>
     </form>
